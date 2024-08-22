9fec5c5ddbc984ced12f1f57bc5b1da7a6d6a1cb
J-Herika <138695574+J-Herika@users.noreply.github.com>
2024-08-22 12:03:01 +0300

    reformated the code

--- script.js
+++ script.js
@@ -4,37 +4,10 @@ const CHOICES = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 
-// 2 - create a function that makes the choices
-function getComputerChoice(choices) {
-  // every time the function is ran i gets a random number between 1-3
-  let randomNum = Math.floor(Math.random() * 3);
-
-  // and then we get the equivalent of that random num in the CHOICES list
-  let choice = choices[randomNum];
-  console.log(choice);
-
-  // and then we return the choice
-  return choice;
-}
-
-function getHumanChoice(choices) {
-  let humanInput = prompt("Choice Rock,Paper or Scissors");
-  humanInput = humanInput.toLowerCase();
-  console.log(humanInput);
-  let choice;
-  switch (humanInput) {
-    case "rock":
-      choice = choices[0];
-      return choice;
-    case "paper":
-      choice = choices[1];
-      return choice;
-    case "scissors":
-      choice = choices[2];
-      return choice;
-    default:
-      alert("You have entered an Invalid input...");
-  }
+function playGame() {
+  humanChoice = getHumanChoice(CHOICES);
+  computerChoice = getComputerChoice(CHOICES);
+  playRound(humanChoice, computerChoice);
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -66,10 +39,29 @@ function playRound(humanChoice, computerChoice) {
   console.log(`player score:${humanScore}\nComputer score: ${computerScore}`);
 }
 
-function playGame() {
-  humanChoice = getHumanChoice(CHOICES);
-  computerChoice = getComputerChoice(CHOICES);
-  playRound(humanChoice, computerChoice);
+function getComputerChoice(choices) {
+  let randomNum = Math.floor(Math.random() * 3);
+  let choice = choices[randomNum];
+  console.log(choice);
+  return choice;
 }
 
-
+function getHumanChoice(choices) {
+  let humanInput = prompt("Choice Rock,Paper or Scissors");
+  humanInput = humanInput.toLowerCase();
+  console.log(humanInput);
+  let choice;
+  switch (humanInput) {
+    case "rock":
+      choice = choices[0];
+      return choice;
+    case "paper":
+      choice = choices[1];
+      return choice;
+    case "scissors":
+      choice = choices[2];
+      return choice;
+    default:
+      alert("You have entered an Invalid input...");
+  }
+}
